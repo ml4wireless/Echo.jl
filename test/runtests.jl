19511cdf27daf26d2ab915af3c334a9fde8aa8b4
Josh Sanz <jsanz@berkeley.edu>
2022-12-16 10:41:45 -0800

    Tighter timing thresholds

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,10 +16,13 @@ function run_converges(configfile)
     @info "Final BERs for $configfile" train_snr_bers
     # Add extra cases for EPP with swapped bit interperpretations
     all(train_snr_bers .< 0.02) || (train_snr_bers[3] < 0.02 && (train_snr_bers[1] > 0.98 || 0.48 < train_snr_bers[1] < 0.52))
+    println()
 end
 
 
 function run_meets_timing(configfile, maxtime)
+    testname = uppercase(basename(configfile)[1:end - 4])
+    @info testname
     cfg = loadconfig(configfile)
     cfg = @set cfg.train_kwargs.num_iterations_train = 100
     ec = ExperimentConfig(cfg, "/tmp")
@@ -32,6 +35,7 @@ function run_meets_timing(configfile, maxtime)
     elapsed = t1 - t0
     @info "Runtime for $configfile" elapsed
     elapsed <= maxtime
+    println()
 end
 
 
@@ -39,7 +43,7 @@ function main()
     helpinfo = "Run with -c to run convergence tests only; -t to run timing tests only; -h to print help"
     # Uncomment to disable progressbar printouts
     # ENV["CI"] = "true"
-    configs = readdir("configs/convergence/", join=true, sort=true)
+    configs = sort(readdir("configs/convergence/", join=true, sort=true))
     nnconf = filter(contains("nn_"), configs)
     ncconf = filter(contains("nc_"), configs)
     ncluconf = filter(contains("nclu_"), configs)
@@ -76,21 +80,24 @@ function main()
     if "-c" ∉ ARGS
         @testset "Run timing" begin
             @testset "NN runtime" begin
-                timings = repeat([2], length(nnconf))
+                # timings = repeat([2], length(nnconf))
+                timings = [2, 1.75, 2.5, 1.25, 1.25, 1]
                 for (t, c) in zip(timings, nnconf)
                     @test run_meets_timing(c, t)
                 end
             end
 
             @testset "NC runtime" begin
-                timings = repeat([2], length(ncconf))
+                # timings = repeat([2], length(ncconf))
+                timings = [1, 1, 1.25, .75, 1, .75]
                 for (t, c) in zip(timings, ncconf)
                     @test run_meets_timing(c, t)
                 end
             end
 
             @testset "NClu runtime" begin
-                timings = repeat([2], length(ncluconf))
+                # timings = repeat([2], length(ncluconf))
+                timings = [3, 1.5, 3, 1.75, 2, 1.5]
                 for (t, c) in zip(timings, ncluconf)
                     @test run_meets_timing(c, t)
                 end
